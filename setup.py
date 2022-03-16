ab8ea46047c924fc01cb5f351bc28449da13003d
Ed Phillips <ed@projectronin.com>
2022-03-16 12:57:54 -0700

    mods for versioning

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup
 
 setup(
     name='infx_api',
-    version='1.2',
     packages=['infx_api'],
+    use_scm_version = {
+        "local_scheme": "no-local-version"
+    },
+    setup_requires=['setuptools_scm'],
 )
